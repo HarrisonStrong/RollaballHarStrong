512d6b01ced1aa710b37eabecf774a8488bf8ba1
James Coddington <james@coddington.io>
2023-01-29 16:53:32 -0600

    Added moving fish and cat photo

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-
+    public float angleSpeed = 30;
+    public float _min = 350;
+    public float _max = 10;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if (transform.eulerAngles.y >= _max && transform.eulerAngles.y <= _min)
+        {
+            angleSpeed *= -1;
+        }
+        transform.Rotate(new Vector3(0, angleSpeed, 0) * Time.deltaTime);
     }
 }
